38951f617bfda674412cfa52dddae5637c9ff9d1
Dagyeong <56528107+DagyeongH@users.noreply.github.com>
2023-09-12 17:48:09 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,16 @@
+def solution(A, B):
+    answer = 0
+    while True:
+        if A == B:
+            break
+        else:
+            if answer == len(A):
+                answer = -1
+                break
+            A = A[-1] + A[-len(A):-1]
+            print(A)
+            answer += 1
+            
+        # 한바퀴 다 돌아도 값이 같지 않다면 빠져나감
+        
+    return answer
